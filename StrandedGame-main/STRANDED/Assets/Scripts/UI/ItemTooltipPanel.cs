739f591aa235656ead81139b98495ca4011b6c46
BilalKhanDev1 <therealbills@hotmail.com>
2022-11-03 01:39:33 -0400

    Toooltip panel disappears when empty slot clicked

--- StrandedGame-main/STRANDED/Assets/Scripts/UI/ItemTooltipPanel.cs
+++ StrandedGame-main/STRANDED/Assets/Scripts/UI/ItemTooltipPanel.cs
@@ -4,19 +4,40 @@ using UnityEngine.UI;
 
 public class ItemTooltipPanel : MonoBehaviour
 {
+    public static ItemTooltipPanel Instance { get; private set; }
+
     [SerializeField] TMP_Text _name;
     [SerializeField] TMP_Text _description;
     [SerializeField] Image _icon;
 
-    public static ItemTooltipPanel Instance { get; private set; }
+    CanvasGroup _canvasGroup;
 
-    void Awake() => Instance = this;
+    void Awake()
+    {
+        Instance = this;
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
 
     public void ShowItem(Item item)
     {
-        _name.SetText(item.name);
-        _description.SetText(item.Description);
-        _icon.sprite = item.Icon;
+        if (item == null)
+        {
+            Toggle(false);
+        }
+        else
+        {
+            Toggle(true);
+            _name.SetText(item.name);
+            _description.SetText(item.Description);
+            _icon.sprite = item.Icon;
+        }
+    }
+
+    void Toggle(bool visible)
+    {
+        _canvasGroup.alpha = visible ? 1f : 0;
+        _canvasGroup.interactable = visible;
+        _canvasGroup.blocksRaycasts = visible;
     }
 
 }
