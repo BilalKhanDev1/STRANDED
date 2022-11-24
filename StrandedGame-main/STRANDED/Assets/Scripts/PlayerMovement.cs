90b19215828e99cc38ee921ee519d90b19381065
Shezwin Carim <shezwin.carim@ontariotechu.net>
2022-11-23 21:07:36 -0500

    adjusted player movement and enemy ai

--- StrandedGame-main/STRANDED/Assets/Scripts/PlayerMovement.cs
+++ StrandedGame-main/STRANDED/Assets/Scripts/PlayerMovement.cs
@@ -2,36 +2,137 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float _turnSpeed = 1000f;
-    [SerializeField] float _moveSpeed = 5f;
+    public bool isMove { get; private set; } = true;
+    private bool isSprinting => Sprinting && Input.GetKey(sprintKey);
+    private bool isJumping => Input.GetKeyDown(jumpKey) && characterController.isGrounded;
+    [Header("Look Settings")]
+    [SerializeField, Range(1, 10)] private float lookSpeedX = 2.0f;
+    [SerializeField, Range(1, 10)] private float lookSpeedY = 2.0f;
+    [SerializeField, Range(1, 180)] private float upperlookLimit = 80.0f;
+    [SerializeField, Range(1, 180)] private float lowerlookLimit = 80.0f;
 
+<<<<<<< Updated upstream
     Rigidbody _rigidbody;
     float _mouseMovement;
+=======
+    [Header("Movement Settings")]
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float sprintSpeed = 6.0f;
+    [SerializeField] float gravity = 9.8f;
+    [SerializeField] float jumpForce = 4.0f;
+    [SerializeField] private bool Sprinting = true;
+    [SerializeField] private bool Jumping = true;
+    [SerializeField] private bool canUseHeadbob = true;
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+>>>>>>> Stashed changes
 
-    void Awake() => _rigidbody = GetComponent<Rigidbody>();
+    [Header("Headbob")]
+    [SerializeField] private float walkBobSpeed = 14f;
+    [SerializeField] private float walkBobValue = 0.05f;
+    [SerializeField] private float sprintBobSpeed = 18f;
+    [SerializeField] private float sprintBobValue = 0.05f;
+    private float defaultYpos = 0;
+    private float timer;
 
+
+   
+    Camera playerCamera;
+    CharacterController characterController;
+
+    private Vector3 moveDir;
+    private Vector2 currInput;
+    
+    float _mouseMovementX = 0;
+   
+
+    void Awake()
+    {
+        
+        playerCamera = GetComponentInChildren<Camera>();
+        characterController = GetComponent<CharacterController>();
+        defaultYpos = playerCamera.transform.localPosition.y;
+    }
+   
+
+<<<<<<< Updated upstream
     void Update() => _mouseMovement += Input.GetAxis("Mouse X");
+=======
+    void Update()
+    {
+        if (ToggleablePanel.AnyVisible != true)
+        Cursor.lockState = CursorLockMode.Locked;
+        if (isMove)
+        {
+            MouseLook();
+            Move();
+          
+
+            if (Jumping)
+                Jump();
+
+            if (canUseHeadbob)
+                HeadBob();
+        }
+        
+    }
+>>>>>>> Stashed changes
 
     void FixedUpdate()
     {
         if (ToggleablePanel.AnyVisible)
             return;
 
+<<<<<<< Updated upstream
         transform.Rotate(0, _mouseMovement * Time.deltaTime * _turnSpeed, 0);
 
         _mouseMovement = 0;
-
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            vertical *= 2f;
+=======
         }
 
-        var velocity = new Vector3(horizontal, 0, vertical);
-        velocity *= _moveSpeed * Time.fixedDeltaTime;
-        Vector3 offset = transform.rotation * velocity;
-
-        _rigidbody.MovePosition(transform.position + offset);
     }
+
+    private void MouseLook()
+    {
+        _mouseMovementX -= Input.GetAxis("Mouse Y") * lookSpeedY;
+        _mouseMovementX = Mathf.Clamp(_mouseMovementX, -upperlookLimit, lowerlookLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(_mouseMovementX,0,0);
+        transform.rotation *= Quaternion.Euler(0,Input.GetAxis("Mouse X") *lookSpeedX,0);   
+        
+    }
+    private void Jump()
+    {
+        if (isJumping)
+            moveDir.y = jumpForce;
+    }
+    private void HeadBob()
+    {
+        if (!characterController.isGrounded) return;
+>>>>>>> Stashed changes
+
+        if (Mathf.Abs(moveDir.x)> 0.1f || Mathf.Abs(moveDir.z) > 0.1f)
+        {
+            timer += Time.deltaTime *  ( isSprinting ? sprintBobSpeed : walkBobSpeed);
+            playerCamera.transform.localPosition = new Vector3(
+                playerCamera.transform.localPosition.x, defaultYpos + Mathf.Sin(timer) * (isSprinting ? sprintBobValue : walkBobValue), playerCamera.transform.localPosition.z);
+
+        }
+    }
+    private void Move()
+    {
+        if (characterController.isGrounded)
+            currInput = new Vector2((isSprinting ? sprintSpeed : moveSpeed) * Input.GetAxis("Vertical"), (isSprinting ? sprintSpeed : moveSpeed) * Input.GetAxis("Horizontal"));
+
+        float moveDirectionY = moveDir.y;
+        if (characterController.isGrounded)
+            moveDir = (transform.TransformDirection(Vector3.forward) * currInput.x) + (transform.TransformDirection(Vector3.right) * currInput.y);
+        moveDir.y = moveDirectionY;
+
+        if (!characterController.isGrounded)
+            moveDir.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDir * Time.deltaTime);
+    }
+    
+
+    
 }
