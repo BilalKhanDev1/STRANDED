5bbf333e2b30db23a3a3f53908fba832bdc8253b
Shezwin Carim <shezwin.carim@ontariotechu.net>
2022-11-30 14:44:40 -0500

    Enemy

--- StrandedGame-main/STRANDED/Assets/Scripts/Enemy/EnemyContoller.cs
+++ StrandedGame-main/STRANDED/Assets/Scripts/Enemy/EnemyContoller.cs
@@ -16,6 +16,16 @@ public class EnemyContoller : MonoBehaviour
     [SerializeField]private Transform player;
     [SerializeField] private Transform target;
 
+
+    public float movementspeed = 8f;
+    public float rotateSpeed = 100f;
+
+    private bool isWandering = false;
+    public bool isRotatingLeft = false;
+    public bool isRotatingRight = false;
+    public bool isWalking = false;
+
+    Rigidbody rb;
     private void Awake()
     {
         
@@ -27,6 +37,22 @@ public class EnemyContoller : MonoBehaviour
 
     private void Update()
     {
+        if(isWandering == false)
+        {
+            StartCoroutine(Wander());
+        }
+        if (isRotatingLeft == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * rotateSpeed);
+        }
+        if (isRotatingRight == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * -rotateSpeed);
+        }
+        if(isWalking == true)
+        {
+            rb.AddForce(transform.forward*movementspeed);
+        }
         if (Vector2.Distance(transform.position, player.position) < attackRange)
         {
             MoveToTarget();
@@ -36,7 +62,7 @@ public class EnemyContoller : MonoBehaviour
     }
     private void MoveToTarget()
     {
-        agent.SetDestination(target.position);
+        agent.SetDestination(player.position);
         Debug.Log("Moving towards player");
         RotateToTarget();
 
@@ -82,10 +108,48 @@ public class EnemyContoller : MonoBehaviour
         stats.DealDamage(statsToDamage);
     }
 
+    IEnumerator Wander()
+    {
+        int rotationTime = Random.Range(1, 3);
+        int rotateWait = Random.Range(1, 3);
+        int rotateDirection= Random.Range(1, 2);
+        int walkWait = Random.Range(1, 3);
+        int walkTime = Random.Range(1, 3);
+
+        isWandering = true;
+        
+        yield return new WaitForSeconds(walkWait);
+
+        isWalking = true;
+
+        yield return new WaitForSeconds(walkTime);
+
+        isWalking=false;
+
+        yield return new WaitForSeconds(rotateWait);
+
+        if(rotateDirection == 1)
+        {
+            isRotatingLeft = true;
+            yield return new WaitForSeconds(rotationTime);
+            isRotatingLeft = false;
+        }
+        if (rotateDirection == 2)
+        {
+            isRotatingRight = true;
+            yield return new WaitForSeconds(rotationTime);
+            isRotatingRight = false;
+        }
+
+        isWandering = false;
+    }
+
     private void GetReferences()
     {
         agent = GetComponent<NavMeshAgent>();
         stats = GetComponent<EnemyStats>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = player;
+        rb = GetComponent<Rigidbody>(); 
     }
 }
